d43572125b083d18913c6d0a42c572d2e3808ccf
Jose Guillermo Avila Griffin <joseph.a.griff@gmail.com>
2019-09-11 16:47:09 -0600

    CardModal Improvements

--- client/src/modules/CardModal.js
+++ client/src/modules/CardModal.js
@@ -21,19 +21,25 @@ class CardModal extends Component {
         <div className="modal fade" id={`cardView${this.props.card._id}`} tabIndex="-1" role="dialog" aria-labelledby={`${this.props.card._id}Label`} aria-hidden="true">
           <div className="modal-dialog modal-lg">
             <div className="modal-content">
-              <div className="modal-header">
+              <div className="modal-header" style={{'borderBottomColor': '#3e5369'}}>
                 <h5 className="modal-title" id={`${this.props.card._id}Label`}>{this.props.card.name}</h5>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div className="modal-body">
-                <p className="subtitle">Description</p>
-                <textarea className="form-control column-name" id={`description_${this.props.card._id}`} onChange={this.props.handleChange} onBlur={this.props.submitCardChange} >{this.props.card.description || ''}</textarea>
-                <p className="subtitle">Activity</p>
-                <p className="content">
-                  {this.state.resolvedActivity}
-                </p>
+              <div className="modal-body row">
+                <div className="col-md-10">
+                  <p>In List ...</p>
+                  <p className="subtitle">Description</p>
+                  <textarea className="form-control card-description" id={`description_${this.props.card._id}`} onChange={this.props.handleChange} onBlur={this.props.submitCardChange} placeholder="Write a description here.">{this.props.card.description || ''}</textarea>
+                  <p className="subtitle">Activity</p>
+                  <p className="content">
+                    {this.state.resolvedActivity}
+                  </p>
+                </div>
+                <div className="col-md-2">
+                  <div>Actions</div>
+                </div>
               </div>
             </div>
           </div>
